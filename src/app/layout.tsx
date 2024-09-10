06965c1625ec3f13bd5b059b6109b28f3f3b4735
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-09-10 18:51:17 +0530

    Change header navigation component folder name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
-import HeaderNavigation from "@/components/layout/headerNavigation";
+import HeaderNavigation from "@/components/navigation/headerNavigation";
 
 const noto_sans = Noto_Sans({ subsets: ["latin"] });
 
