6972c91f03ba34d99818c5e2b507e43e38259a00
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-09-19 18:45:03 +0530

    Fix: Alignment of the overlapping header navigation with the hero section

--- src/components/navigation/headerNavigation.tsx
+++ src/components/navigation/headerNavigation.tsx
@@ -7,7 +7,7 @@ import MobileNavigation from "../component/mobileNavigation";
 
 const headerNavigation: FC = () => {
   return (
-    <header className="lg:sticky lg:top-8 lg:z-10">
+    <header className="lg:sticky lg:top-0 lg:z-10 lg:mx-auto lg:max-w-5xl lg:px-8 lg:py-8">
       <div className="lg:flex lg:justify-center">
         <div className="flex w-full flex-row items-center justify-between gap-4 bg-zinc-800 px-6 py-5 text-zinc-100 antialiased md:px-10 lg:w-fit lg:rounded-2xl lg:border lg:border-zinc-700/60 lg:px-4 lg:py-[10px]">
           <div className="flex items-center justify-center gap-2">
