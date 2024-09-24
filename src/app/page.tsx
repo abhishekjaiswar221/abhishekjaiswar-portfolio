af3aeae2c4bb0a5725d481f13d3fb4b2e21ed469
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-09-24 19:09:38 +0530

    Complete the skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/hero";
 import { cn } from "@/lib/utils";
 import { Andika } from "next/font/google";
 import Projects from "@/components/projects";
+import Skills from "@/components/skills";
 
 const andika = Andika({ subsets: ["latin"], weight: "400" });
 
@@ -11,7 +12,7 @@ const Home: FC = () => {
     <main
       className={cn(
         andika.className,
-        "mx-auto flex max-w-5xl flex-col items-center gap-10 px-6 py-10 md:px-10 md:py-20 xl:px-0"
+        "mx-auto flex max-w-5xl flex-col items-center gap-20 px-6 py-10 md:px-10 md:py-20 xl:px-0"
       )}
     >
       <section id="hero">
@@ -19,6 +20,9 @@ const Home: FC = () => {
       </section>
       <section id="projects">
         <Projects />
+      </section>
+      <section id="skills">
+        <Skills />
       </section>
     </main>
   );
---
+++ src/components/Card/techCard.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { Noto_Sans } from "next/font/google";
+import { TechIconsType } from "@/data/dataTypes";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const noto_sans = Noto_Sans({ subsets: ["latin"] });
+
+interface TechCardProps {
+  iterableArray: Array<TechIconsType>;
+  cardTitle: string;
+}
+
+const TechCard: FC<TechCardProps> = ({ iterableArray, cardTitle }) => {
+  return (
+    <Card className="rounded-lg border-zinc-700/60 bg-zinc-800 shadow-2xl">
+      <CardHeader className="p-4">
+        <CardTitle className="scroll-m-20 text-center text-lg font-semibold tracking-tight text-zinc-400 md:text-xl">
+          {cardTitle}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="grid grid-cols-2 gap-4 p-5 pt-1 md:grid-cols-3 lg:grid-cols-4 lg:gap-3">
+        {iterableArray.map(({ id, title, imgSrc, altText }: TechIconsType) => {
+          return (
+            <div key={id}>
+              <Button className="flex h-10 w-28 flex-row items-center justify-center gap-2 border border-zinc-700/60 bg-[#14131a] shadow-none hover:bg-zinc-700 md:h-full md:w-full md:flex-col">
+                <Image
+                  src={`/techIcons/${imgSrc}`}
+                  alt={altText}
+                  width={20}
+                  height={20}
+                />
+                <span
+                  className={cn(noto_sans.className, "text-xs text-zinc-400")}
+                >
+                  {title}
+                </span>
+              </Button>
+            </div>
+          );
+        })}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TechCard;
