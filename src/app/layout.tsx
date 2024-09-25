70ab371b621feed9eb95c5aa7b0c221952a2cf9c
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-09-25 18:07:29 +0530

    Add the top loading bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Noto_Sans } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import { Noto_Sans } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import HeaderNavigation from "@/components/navigation/headerNavigation";
 import FooterNavigation from "@/components/navigation/footerNavigation";
 
@@ -29,6 +30,21 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
           noto_sans.className
         )}
       >
+        <NextTopLoader
+          color="#6366f1"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={2}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+          showAtBottom={false}
+        />
         <HeaderNavigation />
         {children}
         <FooterNavigation />
