2bc1743f02bb1052c435237226971965302f6c22
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-09-26 17:35:57 +0530

    Add smooth scroll behaviour to the skills section

--- src/components/navigation/footerNavigation.tsx
+++ src/components/navigation/footerNavigation.tsx
@@ -36,7 +36,7 @@ const FooterNavigation: FC = () => {
         </p>
       </div>
       <div>
-        <p className="text-center">Made With ❤ By Abhishek Jaiswar</p>
+        <p className="text-center">Made With ❤️ By Abhishek Jaiswar</p>
       </div>
     </footer>
   );
