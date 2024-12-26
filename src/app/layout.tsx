3f90cdaff738294e54b507b97b5bcf1b13f60662
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-12-26 19:21:47 +0530

    Uninstall vercell web analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,6 @@ import { noto_sans } from "@/lib/fonts";
 import NextTopLoader from "nextjs-toploader";
 import type { Metadata, Viewport } from "next";
 import { Toaster } from "@/components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import HeaderNavigation from "@/components/navigation/headerNavigation";
 import FooterNavigation from "@/components/navigation/footerNavigation";
 
@@ -147,8 +145,6 @@ const RootLayout: FC<Readonly<RootLayoutProps>> = ({ children }) => {
         />
         <HeaderNavigation />
         {children}
-        <Analytics />
-        <SpeedInsights />
         <Toaster />
         <FooterNavigation />
       </body>
